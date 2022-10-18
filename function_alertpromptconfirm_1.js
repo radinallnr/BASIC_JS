9bc454da717d347580d51e361deb7d36a46e860b
Radinal <radinallsnr@gmail.com>
2022-10-18 13:24:16 +0700

    18-10-22_function

---
+++ function_alertpromptconfirm_1.js
@@ -0,0 +1,7 @@
+// xcample
+alert("Do you really want to leave this page?");
+// line breaks
+alert("Hello\nHow are you");
+// prompt
+var user = prompt("please enter your name");
+alert(user);
---
+++ whileloop_program-timer.js
@@ -0,0 +1,22 @@
+// while
+var i=5
+while(i>=0) {
+    console.log(i);
+    i--;
+}
+// example
+var i=1
+var j=1
+// dow hile
+do{
+    console.log(i)
+     i+=1
+}while(i<=4)
+// while
+while(j<=4) {
+    console.log(j)
+    j+=1
+} 
+
+// while step by step
+// do while langsung baru periksa bener/salah
